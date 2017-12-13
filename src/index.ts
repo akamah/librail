6c24cafd0ff9b7f5fa5505b2ac5394a6c73807fa
Akama Hitoshi <akamahitoshi@gmail.com>
2017-12-13 19:47:48 +0900

    change index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
-import { Dir } from "./Dir";
-import { End } from "./End";
-import { Point } from "./Point";
-import { Pole } from "./Pole";
-import { Rot } from "./Rot";
-import { Flip } from "./Flip";
-import { Rail, Straight, Curve, Slope } from "./Rail";
-import { Pier, MiniPier } from "./Pier"
-
-export { Dir, End, Point, Pole, Rot, Rail, Flip }
-export { Pier, MiniPier }
-export { Straight, Curve, Slope }
+export * from "./Dir";
+export * from "./End";
+export * from "./Point";
+export * from "./Pole";
+export * from "./Rot";
+export * from "./Flip";
+export * from "./Rail";
+export * from "./Pier"
