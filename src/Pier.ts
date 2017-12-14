25a50c7e5b8d5a5901f2d41f6b3199b34cb81784
Akama Hitoshi <akamahitoshi@gmail.com>
2017-12-14 22:03:11 +0900

    thinking about piers

--- src/Pier.ts
+++ src/Pier.ts
@@ -12,10 +12,39 @@ export class Pier {
     public height = 4;
 
     constructor(public origin: Point, public dir: Dir) {
+        this.dir = new Dir(this.dir.dir % 4);
     }
 }
 
 export class MiniPier extends Pier {
-    public name = "mini pier"
-    public height = 1;
+    name = "mini pier"
+    height = 1;
+}
+
+export class CustomPier extends Pier {
+    public name = "CustomPier";
+
+    constructor(origin: Point, dir: Dir, public floors: number[]) {
+        super(origin, dir);
+    }
+
+    public equivalentPiers(): Pier[] {
+        var piers: Pier[] = [];
+        var current = 0;
+
+        this.floors.sort().forEach(n => {
+            // n階層になるまで建て続ける
+            for (; current + 4 <= n; current += 4) {
+                let e = new Point(this.origin.single, this.origin.double, current);
+                piers.push(new Pier(e, this.dir));
+            }
+            for (; current + 1 <= n; current += 1) {
+                let e = new Point(this.origin.single, this.origin.double, current);
+                piers.push(new MiniPier(e, this.dir));
+            }
+            // n階になった．
+        });
+
+        return piers;
+    }
 }
